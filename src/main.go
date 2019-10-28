b8865490966ad1cb72559a368e6b542b4dd65060
Sean Hellum <seanhellum45@gmail.com>
2019-10-28 01:03:54 +0000

    Make code skeleton

---
+++ src/main.go
@@ -0,0 +1,39 @@
+package main
+
+type King struct {
+	// WIP
+}
+type Queen struct {
+	// WIP
+}
+type Pawn struct {
+	// WIP
+}
+type Rook struct {
+	// WIP
+}
+type Knight struct {
+	// WIP
+}
+type Bishop struct {
+	// WIP
+}
+
+func main() {
+	// WIP
+}
+func mainMenu() {
+	// WIP
+}
+func newBoard() {
+	// WIP
+}
+func getMove() {
+	// WIP
+}
+func makeMove() {
+	// WIP
+}
+func prettyPrintBoard() {
+	// WIP
+}
