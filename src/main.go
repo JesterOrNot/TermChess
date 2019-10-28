4794014ad9ef658359349c2b74c8ec9a4e2e2971
Sean Hellum <seanhellum45@gmail.com>
2019-10-28 01:40:03 +0000

    code skele update

--- src/main.go
+++ src/main.go
@@ -1,4 +1,16 @@
 package main
+import (
+  "log"
+  "os/exec"
+)
+
+func execCommand() string {
+	output, err := exec.Command("ls", "-l").CombinedOutput()
+	if err != nil {
+		log.Print(err)
+	}
+	return string(output)
+}
 
 type King struct {
 	// WIP
@@ -25,8 +37,16 @@ func main() {
 func mainMenu() {
 	// WIP
 }
-func newBoard() {
-	// WIP
+func newBoard() [][]string {
+	var board = [][]string{{"BR","BN","BB","BQ","BK","BB","BN","BR"},
+						   {"BP","BP","BP","BP","BP","BP","BP","BP"},
+						   {"XX","XX","XX","XX","XX","XX","XX","XX"},
+						   {"XX","XX","XX","XX","XX","XX","XX","XX"},
+						   {"XX","XX","XX","XX","XX","XX","XX","XX"},
+						   {"XX","XX","XX","XX","XX","XX","XX","XX"},
+						   {"WP","WP","WP","WP","WP","WP","WP","WP"},
+						   {"WR","WN","WB","WQ","WK","WB","WN","WR"}}
+	return board
 }
 func getMove() {
 	// WIP
