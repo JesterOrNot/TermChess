6647c1d2842dbec3a6af77c7324d3f1e1ddc6f7b
Sean Hellum <seanhellum45@gmail.com>
2019-12-27 20:26:35 +0000

    update

--- JavaChess/Board.java
+++ JavaChess/Board.java
@@ -7,29 +7,42 @@ public class Board {
 
     // Instance Variables
 
-    private String[][] board = new String[7][7];
-
+    private Piece[][] board = new Piece[7][7];
 
     public Board() {
-        this.board = new String[][]{{"BR", "BN", "BB", "BQ", "BK", "BB", "BN", "BR"},
-                                    {"BP", "BP", "BP", "BP", "BP", "BP", "BP", "BP"},
-                                    {"XX", "XX", "XX", "XX", "XX", "XX", "XX", "XX"},
-                                    {"XX", "XX", "XX", "XX", "XX", "XX", "XX", "XX"},
-                                    {"XX", "XX", "XX", "XX", "XX", "XX", "XX", "XX"},
-                                    {"XX", "XX", "XX", "XX", "XX", "XX", "XX", "XX"},
-                                    {"WP", "WP", "WP", "WP", "WP", "WP", "WP", "WP"},
-                                    {"WR", "WN", "WB", "WQ", "WK", "WB", "WN", "WR"}};
+        this.board = new Piece[][] {
+                    {
+                        new Rook(5, false), new Knight(3, false), new Bishop(3, false), new Queen(9, false),
+                        new King(-1, false), new Bishop(3, false), new Knight(3, false), new Rook(5, false)
+                    },
+                    {
+                        new Pawn(1, false), new Pawn(1, false), new Pawn(1, false), new Pawn(1, false),
+                        new Pawn(1, false), new Pawn(1, false), new Pawn(1, false), new Pawn(1, false)
+                    },
+                    { null, null, null, null, null, null, null, null }, 
+                    { null, null, null, null, null, null, null, null },
+                    { null, null, null, null, null, null, null, null }, 
+                    { null, null, null, null, null, null, null, null },
+                    { 
+                        new Pawn(1, true), new Pawn(1, true), new Pawn(1, true), new Pawn(1, true),
+                        new Pawn(1, true), new Pawn(1, true), new Pawn(1, true), new Pawn(1, true)
+                    },
+                    { 
+                        new Rook(5, true), new Knight(3, true), new Bishop(3, true), new Queen(9, true),
+                        new King(-1, true), new Bishop(3, true), new Knight(3, true), new Rook(5, true)
+                    }
+                };
     }
 
     // Getters
 
-    public String[][] getBoard() {
+    public Piece[][] getBoard() {
         return this.board;
     }
 
     // Setters
 
-    public void setBoard(String[][] board) {
+    public void setBoard(Piece[][] board) {
         this.board = board;
     }
 
